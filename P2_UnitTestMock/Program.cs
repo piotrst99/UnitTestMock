61d2c239357c558b2806d956964485885fde8ee4
piotrst99 <piotrstukus@outlook.com>
2022-11-13 15:49:40 +0100

    Correcr Code

--- P2_UnitTestMock/Program.cs
+++ P2_UnitTestMock/Program.cs
@@ -10,30 +10,3 @@ namespace P2_UnitTestMock {
         }
     }
 }
-
-// https://www.modestprogrammer.pl/jak-pozbywac-sie-zewnetrznych-zaleznosci-w-testach-jednostkowych-wprowadzenie-do-mockowania-danych-w-csharp
-
-// https://ucgosu.pl/2017/09/mocki-radzenie-zaleznosciami-testach/
-
-// https://4programmers.net/Forum/Java/252552-mockowanie_klasy
-
-// https://www.telerik.com/blogs/how-to-simplify-your-csharp-unit-testing-mocking-framework
-
-// https://www.youtube.com/watch?v=DwbYxP-etMY
-
-// https://bd90.pl/net-core-mockowanie-testach-biblioteka-moq/
-
-// https://softchris.github.io/pages/dotnet-moq.html#the-api-project
-
-// moq not using files
-// https://code.4noobz.net/c-moq-tutorial-for-beginners/
-
-// https://dariuszwozniak.net/posts/kurs-tdd-15-wstep-do-moq
-
-// https://methodpoet.com/unit-testing-with-moq/
-
-// https://www.youtube.com/watch?v=9ZvDBSQa_so
-
-// https://www.youtube.com/watch?v=SHAKzgYtu9E
-
-// c# unit testing with Moq
